4af92f073fd04a57833436680afec946f3e1a8e0
Issam Rafie <rafieissam@gmail.com>
2024-01-29 23:05:56 +0200

    add temp colors + icons + update logos

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideNgIconsConfig } from '@ng-icons/core';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideNgIconsConfig({
+      size: "1.2rem",
+      color: "#f4f3ee",
+    }),
+    provideRouter(routes),
+  ]
 };
