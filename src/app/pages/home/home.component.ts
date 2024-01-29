59c93cdcd7b7d1615b60d09d1d43a3977502997a
Issam Rafie <rafieissam@gmail.com>
2024-01-29 23:08:51 +0200

    update home design with layout and add music player

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { jamSearch, jamUserCircle } from '@ng-icons/jam-icons';
+import { MusicPlayerComponent } from '../../components/music-player/music-player.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    NgIconComponent,
+    MusicPlayerComponent,
+  ],
+  providers: [provideIcons({ jamSearch, jamUserCircle })],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  recents = ['1', '2', '3'];
 }
