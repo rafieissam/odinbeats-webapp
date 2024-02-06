42cdb5d5788308ad1198caa7ecbe0d6dc05e76fe
Issam Rafie <rafieissam@gmail.com>
2024-02-06 21:28:59 +0200

    change app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'odinbeats-fe' title`, () => {
+  it(`should have the 'odinbeats-webapp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('odinbeats-fe');
+    expect(app.title).toEqual('odinbeats-webapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, odinbeats-fe');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, odinbeats-webapp');
   });
 });
